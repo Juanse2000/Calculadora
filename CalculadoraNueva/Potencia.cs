0f031a14f9538e3684fe2d84bb8d73ceace7e64c
Juan Garcia <sebacho2906@gmail.com>
2023-06-26 09:53:12 -0500

    Se añade logica de potencia

---
+++ CalculadoraNueva/Potencia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraNueva
+{
+    public class Potencia
+    {
+        public Potencia()
+        {
+
+        }
+        public double Potenciar(double numero1, double numero2)
+        {
+
+            try
+            {
+                double resultado = 0;
+                resultado = Math.Pow(numero1, numero2);
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
